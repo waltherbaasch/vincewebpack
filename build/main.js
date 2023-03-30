d536265e2d87ad87644ff92c6d61f9f6257c964a
waltherbaasch <U2444127@rts-365.dk>
2023-03-30 09:14:17 +0200

    added js

--- build/main.js
+++ build/main.js
@@ -1,1 +1,11 @@
 document.body.appendChild(function(){const e=document.createElement("div");return e.innerHTML="Hello, webpack!",e}());
+async function getRandomJoke() {
+    const url = 'https://icanhazdadjoke.com/';
+    const headers = {
+      'Accept': 'application/json'
+    };
+    const response = await fetch(url, { headers });
+    const data = await response.json();
+    document.getElementById('joke').textContent = data.joke;
+  }
+  
